d1021ee56e6d7d22033381304f5637e51c872823
D Dranidis <dranidis@gmail.com>
2022-02-18 08:56:36 +0200

    Refactor

--- src/bbg_api/db.clj
+++ src/bbg_api/db.clj
@@ -1,22 +1,136 @@
 (ns bbg-api.db
-  (:require [bbg-api.core :as g]
+  (:require [clojure.xml :as xml]
             [clojure.pprint :as pp]))
+;; 
+;; Fields accessors from API XML
+;; 
+(defn- game-id [game]
+  (get-in game [:attrs :objectid]))
 
-(defn collection-game->game
+(defn- game-my-rating [collection-game]
+  (let [rating (-> collection-game
+                   :content
+                   (nth 4)
+                   :content
+                   first
+                   :attrs
+                   :value
+                   read-string)]
+    (if (number? rating) rating nil)))
+
+(defn- game-rating [collection-game]
+  (let [rating (-> collection-game
+                   :content
+                   (nth 4)
+                   :content
+                   first
+                   :content
+                   (nth 2)
+                   :attrs
+                   :value
+                   read-string)]
+    (if (number? rating) rating nil)))
+
+(defn- game-name [collection-game]
+  (-> collection-game
+      :content
+      first
+      :content
+      first))
+
+(defn- game-attributes [collection-game]
+  (-> collection-game
+      :content
+      (nth 4)
+      :attrs))
+
+(defn- game-attribute [collection-game]
+  (fn [key]
+    (let [attr (game-attributes collection-game)
+          value (attr key)]
+      (if value (read-string value) nil))))
+
+;; (defn game-playingtime [collection-game]
+;;   ((game-attribute collection-game) :playingtime))
+
+;; (defn game-maxplayers [collection-game]
+;;   ((game-attribute collection-game) :maxplayers))
+
+;; (defn game-minplayers [collection-game]
+;;   ((game-attribute collection-game) :minplayers))
+
+;; 
+;; Game api
+;; 
+(defn api-read-game [game-id]
+  (Thread/sleep 1000)
+  (-> (xml/parse (str "https://boardgamegeek.com/xmlapi/boardgame/" game-id))
+      :content
+      first))
+
+(defn get-games-and-write-to-file [collection]
+  (spit "resources/games.clj"
+        (reduce
+         #(assoc %1 (get-in %2 [:attrs :objectid]) %2)
+         {}
+         (map api-read-game (map game-id collection)))))
+
+(defn read-games-from-file []
+  (read-string (slurp "resources/games.clj")))
+
+;; 
+;; Collection API 
+;; 
+(defn fetch-collection-and-write-to-file [user-name]
+  (spit "resources/collection.clj"
+        (xml/parse (str "https://boardgamegeek.com/xmlapi/collection/" user-name))))
+
+(defn read-collection-from-file []
+  (:content (read-string (slurp "resources/collection.clj"))))
+
+;; 
+;; Functions for numbers of players
+;; 
+(defn- polls-with-num-of-players-for-game [game]
+  (let [tag-list (game :content)
+        tag-poll (filter (fn [x] (= (x :tag) :poll)) tag-list)
+        recommended (filter (fn [x] (= (get-in x [:attrs :name]) "suggested_numplayers")) tag-poll)]
+    ((first recommended) :content)))
+
+(defn- votes-best-rating-per-players [data]
+  (let [players (get-in data [:attrs :numplayers])
+        total-votes (apply + (map (fn [x] (read-string (get-in x [:attrs :numvotes]))) (data :content)))
+        best-votes (read-string (get-in (first (data :content)) [:attrs :numvotes]))
+        best-perc (if (= 0 total-votes) 0 (/ best-votes total-votes))
+
+        recommended-votes (read-string (get-in (second (data :content)) [:attrs :numvotes]))
+        recommended-perc (if (= 0 total-votes) 0 (/ recommended-votes total-votes))
+        not-recommended-votes (read-string (get-in (last (data :content)) [:attrs :numvotes]))
+        not-recommended-perc (if (= 0 total-votes) 0 (/ not-recommended-votes total-votes))]
+    {:players players
+     :best-votes best-votes :best-perc best-perc
+     :recommended-votes recommended-votes :recommended-perc recommended-perc
+     :not-recommended-votes not-recommended-votes :not-recommended-perc not-recommended-perc}))
+
+
+;; 
+;; API - DB
+;; 
+(defn- collection-game->game
   [games collection-game]
-  (let [game-id (g/game-id collection-game)
-        votes (map g/votes-best-rating-per-players
-                   (g/polls-with-num-of-players-for-game (games game-id)))]
+  (let [game-id (game-id collection-game)
+        votes (map votes-best-rating-per-players
+                   (polls-with-num-of-players-for-game (games game-id)))]
     {:id game-id
-     :name (g/game-name collection-game)
-     :rating (g/game-rating collection-game)
-     :my-rating (g/game-my-rating collection-game)
-     :minplayers ((g/game-attribute collection-game) :minplayers)
-     :maxplayers ((g/game-attribute collection-game) :maxplayers)
-     :playingtime ((g/game-attribute collection-game) :playingtime)
+     :name (game-name collection-game)
+     :rating (game-rating collection-game)
+     :my-rating (game-my-rating collection-game)
+     :minplayers ((game-attribute collection-game) :minplayers)
+     :maxplayers ((game-attribute collection-game) :maxplayers)
+     :playingtime ((game-attribute collection-game) :playingtime)
      :votes votes}))
 
-(defn make-db-from-collection-and-games
+(defn- make-db-from-collection-and-games
   [collection games]
   (let [all (map #(collection-game->game games %) collection)
         all-db (reduce
@@ -24,11 +138,11 @@
                 {} all)]
     (spit "resources/db.clj" (with-out-str (pp/pprint all-db)))))
 
-(defn make-db 
+(defn make-db
   []
-  (make-db-from-collection-and-games 
-   (g/read-collection-from-file) 
-   (g/read-games-from-file)))
+  (make-db-from-collection-and-games
+   (read-collection-from-file)
+   (read-games-from-file)))
 
 (defn read-db
   []
